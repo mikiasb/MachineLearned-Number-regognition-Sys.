2f11542b1a72b2ffaecd7e30fba29b2c6dcddd33
Mikias S. Berhane <mikiasber12@gmail.com>
2022-06-25 06:56:47 -0700

    first commit

---
+++ views.py
@@ -0,0 +1,25 @@
+import matplotlib
+from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+import numpy as np
+import matplotlib.pyplot as pit 
+
+digits = load_digits()
+
+(X_train, X_test, y_train, y_test) = train_test_split(
+    digits.data, digits.target, test_size=0.25, random_state=42
+)
+
+ks = np.arange(2, 10)
+scores = []
+for k in ks:
+    model = KNeighborsClassifier(n_neighbors=k)
+    score = cross_val_score(model, X_train, y_train, cv=5)
+    score.mean()
+    scores.append(score.mean())
+
+pit.plot(scores, ks)
+pit.xlabel('accuracy')
+pit.ylabel('k')
+pit.show()
